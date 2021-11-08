ef7da09f5707522bdd8e957b304e08404f00060c
Andy <53621620+JiananYuan@users.noreply.github.com>
2021-11-08 21:12:57 +0800

    Update Merkle 树.java

--- Merkle 树.java
+++ Merkle 树.java
@@ -6,30 +6,37 @@ import java.util.Scanner;
 public class Merkle {
 
     public static void main(String[] args) {
+        // 用户输入的数据个数
         int n;
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
-        int e = 1;
-        while (n < (1 << e)) {
+        // 规整到2的整数次幂
+        int e = 0;
+        while (n > (1 << e)) {
             e += 1;
         }
         int nn = (1 << e);
+        // 存储数据
         int[] data = new int[nn + 1];
         for (int i = 1 ; i <= n; i += 1) {
             data[i] = in.nextInt();
         }
+        // 构造Merkel 树存储数组
         int totalNodes = (1 << (e + 1)) - 1;
         String[] merkleTree = new String[totalNodes + 1];
+        // 构造底层哈希
         int ptr = 1;
         for (int i = totalNodes - nn + 1; i <= totalNodes; i += 1) {
             merkleTree[i] = encrypt("" + data[ptr]);
             ptr += 1;
         }
+        // 自低向上计算hash 值
         int left = totalNodes - nn + 1;
         int right = totalNodes;
         // 到merkle 的根节点停止hash 计算
         while (left != 1) {
             for (int i = left; i <= right; i += 2) {
+                // todo: 拼接在一起哈希?
                 merkleTree[i / 2] = encrypt(merkleTree[i] + merkleTree[i + 1]);
             }
             left >>= 1;
@@ -38,7 +45,11 @@ public class Merkle {
         System.out.println("finger_print 为：" + merkleTree[1]);
     }
 
+    // SHA-256 加密入参
     public static String encrypt(String value) {
+        if (value == null) {
+            return "";
+        }
         MessageDigest messageDigest = null;
         byte[] buf = null;
         try {
